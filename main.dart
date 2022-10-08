f523fa58c3258460edb87483e95220725a4fc6f0
ahsan ali <57295435+AhsanAliProgrammer@users.noreply.github.com>
2022-10-08 14:34:09 +0000

    Update main.dart

--- main.dart
+++ main.dart
@@ -60,5 +60,39 @@ void main(){
       elseif(temperature > 10){
 
       };
+    // Question#6
+
+  // var c = "a";
+  // if (c == "a" || c == "e" || c == "i" || c == "o" || c == "u") {
+  //   print("lowel-case vowel");
+  // }elseif(c == "A"||c == "E"||c == "I"||c == "O"||c == "U"){
+  //   print("uper-case vowel");
+  // }
+
+  // Question#7
+
+  // var name = "ahsan";
+  // var cusid = 1001;
+  // var unit = 800;
+  // var charges = 1.20;
+  // if (unit < 200) {
+  //   print("charges:$charges");
+  // }elseif(cusid>=200&&cusid<400){
+  //   print("charges:$charges");
+  // }elseif(cusid>=400&&cusid<600){
+  //   print("charges:$charges");
+  // }else{
+  //   print("charges:$charges");
+  // }
+  // var totalamount=cusid*charges;
+  // var surchg=totalamount*15/100.0;
+  // if(totalamount>300){
+  //   print("surchage amount:$surchg");
+  // }
+  // var netamount= totalamount+surchg;
+  // if(netamount<100){
+  //   netamount=100;
+  //   print("net amount paid by the customer:$netamount");
+  // }
       
 }
