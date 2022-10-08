ceab936098c9d89d0f14485988b0d913eba0c67a
Ahsanaliprogrammer <ahsanmehboob16@gmail.com>
2022-10-08 16:10:36 +0500

    initial commit

---
+++ main.dart
@@ -0,0 +1,64 @@
+void main(){
+  // Question#1
+
+  // int l=2;
+  // int b=2;
+  //  if(l==b){
+  //   print("square value");
+  
+  //  }else{
+  //   print("rectangle value");
+  //  }
+      
+      // Question#2
+
+      // int a=25;
+      // int b=25;
+      // int c=30;
+      // if((a > b) && (a>c)){
+      //   print("oldest");
+      // }else{
+      //   print("youngest");
+      // }
+      // if((b>a)&&(b>a)){
+      //   print("oldest");
+      // }else{
+      //   print("youngest");
+      // }
+      // if(c>a&&c>a){
+      //   print("oldest");
+
+      // }else{
+      //   print("youngest");
+      // }
+
+      // Question#3
+      // int a=15;
+      // int b=10;
+      // double c=(b/a)*100;
+      // if(c>=75){
+      //   print("your atttance percentage is:$c");
+      //   print("you are eligible for giving the examination:");
+      // }else{
+      //   print("sorry! you are not eligible for giving examination");
+      // }
+
+      // Question#4
+      // int year =2000;
+      // if((year%4==0 && year%100!=0)||year%400==0){
+      //   print("is a leap year");
+      // }
+      // else{
+      //   print("is not a leap year");
+      // }
+
+      // Question#5
+      int temperatue=42;
+      if(temperatue<0){
+        print("freezing weather");
+      }
+      elseif(temperature > 10){
+
+      };
+      
+}
